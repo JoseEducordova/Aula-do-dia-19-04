1de86edd3a9328f687b08501848d7511d9c2bec6
JoseEducordova <josejojocarvalho@gmail.com>
2023-04-19 21:55:48 -0300

    subindo exercicio da aula 19/04

---
+++ index.js
@@ -0,0 +1,49 @@
+//let nome = null
+//let idade = null
+//console.log(typeof nome)
+//console.log(typeof idade)
+//Ao que me aparenta, quando não se possui nenhum valor incluido a variavel, ele acaba por se tornar um objeto
+
+//nome = prompt ("Digite seu nome: ")
+//idade = Number( prompt ("Sua idade: "))
+//console.log(idade)
+//console.log(typeof nome)
+//console.log(typeof idade)
+//ao utilizar o codigo que aprendemos nesta aula, notei que ao invés de o carcter idade ser considerado um string, foi considerado um caracter number, que é sua classificação correta.
+
+//nome = prompt ("Digite seu nome: ")
+//idade = Number( prompt ("Sua idade: "))
+//console.log(nome)
+//console.log(idade)
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log("ola " + nome + " você tem " + idade + " anos")
+
+//const resposta1 = prompt ("Você gosta de pizza? ")
+//const resposta2 = prompt ("Você gosta de video games? ")
+//const resposta3 = prompt ("Você gosta de sorvete? ")
+//console.log ("Você gosta de pizza? " + resposta1 )
+//console.log ("Você gosta de video games? " + resposta2 )
+//console.log ("Você gosta de sorvete? " + resposta3 )
+
+//let a = 10
+//let b = 25
+//let c = a
+ 
+//a = b
+//b = c
+//c = a
+
+//let a = 10
+//let b = 25
+//let c = a
+
+//console.log("O novo valor de a é", b)
+//console.log("O novo valor de b é", c)
+
+//const numero1 = Number(prompt ("Digite um número: "))
+//const numero2 = Number(prompt ("Digite um segundo número: "))
+//let resultadoX = numero1 + numero2
+//let resultadoY= numero1 * numero2 
+//console.log("A soma dos valores anteriores é de: " + resultadoX)
+//console.log("A multiplicação dos valores anteriores é de: " + resultadoY)
